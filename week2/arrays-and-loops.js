e5ecc4e7f9def46f29b9d55b4bdba6b09d4a1809
dcruz354 <dcruz354@gmail.com>
2021-05-23 14:31:26 -0700

    update arrays and loop js file

--- week2/arrays-and-loops.js
+++ week2/arrays-and-loops.js
@@ -1,6 +1,8 @@
 // #1 Take an array of numbers and make them strings
 function stringItUp(arr) {
 	// your code here
+	var map1 = arr.map(item => item.toString());
+	return map1;
 }
 
 console.log(stringItUp([2, 5, 100]));
@@ -9,6 +11,9 @@ console.log(stringItUp([2, 5, 100]));
 // #2 Make an array of strings of the names
 function namesOnly(arr) {
 	// your code here
+	var map2 = arr.map(item => item.name);
+	return map2;
+
 }
 
 console.log(
@@ -40,6 +45,8 @@ console.log(
 // #3 Filter the output array from #2.
 function oldOnesOnly(arr) {
 	// your code here
+	var map3 = arr.map(item => item.age).filter(age => age >= 80);
+	return map3;
 }
 console.log(
 	oldOnesOnly([
